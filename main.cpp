b8a230962db889a32faaac9dceb392cf8bc3306f
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-03 14:25:58 -0600

    Basic main header file and main.cpp created.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*********************
+* Name: Graziella Buitron
+* Coding 02
+* Purpose: To demonstrate (and advance) your knowledge of classes, header files, objects,
+* compiling, version control, and GitHub. Using example of circle and sphere. 
+**********************/
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+
+
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+/*********************
+* Name: Graziella Buitron
+* Coding 02
+* Purpose: To demonstrate (and advance) your knowledge of classes, header files, objects,
+* compiling, version control, and GitHub. Using example of a circle and sphere. 
+**********************/
+
+#ifndef MAIN_CLASSESPRACTICE_H
+#define MAIN_CLASSESPRACTICE_H
+
+
+#endif /* MAIN_CLASSESPRACTICE_H */
