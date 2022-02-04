350c8f7b315a972dcd4cca3b9ff590b54d25d0f4
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-03 21:45:34 -0600

    initial commit of pyramid header and pyramid.cpp files

---
+++ pyramid.h
@@ -0,0 +1,14 @@
+/*********************
+* Name: Graziella Buitron
+* Coding 02
+* Purpose: To demonstrate (and advance) your knowledge of classes, header files, objects,
+* compiling, version control, and GitHub. 
+**********************/
+
+
+#ifndef CLASSES_PYRAMID_H
+#define CLASSES_PYRAMID_H
+
+#include "triangle.h"
+
+#endif //CLASSES_PYRAMID_H
