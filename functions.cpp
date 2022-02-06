ee2357dbc52ea1bbc0750c226df11f268ad1c21d
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-06 03:15:38 -0600

    Made edits to display text. Added line to display pyramid area

--- functions.cpp
+++ functions.cpp
@@ -11,10 +11,13 @@ void displayTriangle(Triangle *myTriangle){
     std::cout << "Triangle base: " << myTriangle->getBase() << std::endl;
     std::cout << "Triangle height: " << myTriangle->getHeight() << std::endl;
     std::cout << "Triangle area: " << myTriangle->getArea() << std::endl;
+    std::cout << std::endl;
 }
 
 void displayPyramid(Pyramid *myPyramid){
-    std::cout << "Pyramid base: " << myPyramid->getBase() << std::endl;
+    std::cout << "Pyramid length: " << myPyramid->getLength() << std::endl;
+    std::cout << "Pyramid width: " << myPyramid->getWidth() << std::endl;
     std::cout << "Pyramid height: " << myPyramid->getHeight() << std::endl;
     std::cout << "Pyramid area: " << myPyramid->getArea() << std::endl;
+    std::cout << std::endl;
 }
