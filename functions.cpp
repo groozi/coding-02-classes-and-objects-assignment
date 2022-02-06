f265fce9398d2e186eae0586e39d63aa616f0023
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-06 02:45:45 -0600

    initial commit of functions.h and functions.cpp file

---
+++ functions.cpp
@@ -0,0 +1,8 @@
+/*********************
+* Name: Graziella Buitron
+* Coding 02
+* Purpose: To demonstrate (and advance) your knowledge of classes, header files, objects,
+* compiling, version control, and GitHub. 
+**********************/
+
+#include "functions.h"
---
+++ functions.h
@@ -0,0 +1,11 @@
+/*********************
+* Name: Graziella Buitron
+* Coding 02
+* Purpose: To demonstrate (and advance) your knowledge of classes, header files, objects,
+* compiling, version control, and GitHub. 
+**********************/
+
+#ifndef CLASSES_FUNCTIONS_H
+#define CLASSES_FUNCTIONS_H
+
+#endif //CLASSES_FUNCTIONS_H
