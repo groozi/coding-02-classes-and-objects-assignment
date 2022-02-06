a6551b35a2cd58ac89c37cb909b5c2b23d1c4c34
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-06 02:54:33 -0600

    Added #include iostream and pyramid.h file. Added function prototypes

--- functions.h
+++ functions.h
@@ -8,4 +8,10 @@
 #ifndef CLASSES_FUNCTIONS_H
 #define CLASSES_FUNCTIONS_H
 
+#include <iostream>
+#include "pyramid.h"
+
+void displayTriangle(Triangle*);
+void displayPyramid(Pyramid*);
+
 #endif //CLASSES_FUNCTIONS_H
