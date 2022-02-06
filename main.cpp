ac3dc77405660ac3c12e7ccc95619541976967e0
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-02-06 03:30:05 -0600

    Testing classes and function calls with negative values to test validation

--- main.cpp
+++ main.cpp
@@ -9,20 +9,19 @@
 
 int main(int argc, char** argv) {
 
-	//creating objects. testing overloaded functions
-	Triangle myTriangle1;
-	Triangle myTriangle2(1, 3);
+    //creating objects. testing overloaded functions
+    Triangle myTriangle1;
+    Triangle myTriangle2(1, -2);
 
-	Pyramid myPyramid1;
-	Pyramid myPyramid2(8, 6, 5);
+    Pyramid myPyramid1;
+    Pyramid myPyramid2(-2, -1, 5);
 
-	//displays results
-	displayTriangle(&myTriangle1);
-	displayTriangle(&myTriangle2);
+    //displays results
+    displayTriangle(&myTriangle1);
+    displayTriangle(&myTriangle2);
 
-	displayPyramid(&myPyramid1);
-	displayPyramid(&myPyramid2);
+    displayPyramid(&myPyramid1);
+    displayPyramid(&myPyramid2);
 
     return 0;
-
 }
